82e4cbdd0a4cde8899b6c8a18fd8b9b46fb33c5e
amrish24 <amrish123@gmail.com>
2014-12-02 12:37:35 -0800

    Create Counter.java

---
+++ Counter.java
@@ -0,0 +1,72 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Counter extends Actor
+{
+    private static final Color textColor = new Color(29, 234, 59);
+
+    public static int value;
+    private static int target = 0;
+    private String text;
+    private int stringLength;
+    
+    public static int levelDelay;
+    
+
+    public Counter()
+    {
+        this("");
+    }
+
+    public Counter(String prefix)
+    {
+        text = prefix;
+        stringLength = (text.length() + 2) * 10;
+
+        setImage(new GreenfootImage(stringLength, 16));
+        GreenfootImage image = getImage();
+        image.setColor(textColor);
+
+        value = 0;
+        target = 0;
+        levelDelay = 0;
+        updateImage();
+        
+    }
+
+    public void act() {
+        updateImage();
+        levelDelay++;
+    }
+
+    public static void add(int score)
+    {
+        value += score;
+    }
+
+    public int getValue()
+    {
+        return target;
+    }
+
+    /**
+     * Make the image
+     */
+    private void updateImage()
+    {
+        GreenfootImage image = getImage();
+        image.clear();
+        image.drawString(text + value, 1, 12);
+    }
+
+    public void setValue(int newValue)  
+    {  
+        target = newValue;  
+        value = newValue;  
+        updateImage();  
+    }  
+    
+}
